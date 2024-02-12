53fba9065c19f8b26d6d95db3b58ecef65113fa0
masssssha <mehovamaria@yandex.ru>
2024-02-12 21:38:02 +0300

    stacklst

--- prj.lab/stacklst/stacklst.cpp
+++ prj.lab/stacklst/stacklst.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 
 const Complex& StackLst::Top() {
+  if (head_ == nullptr) {
+    throw std::logic_error("Stack is empty");
+  }
   return (head_->v);
 }
 
@@ -13,9 +16,11 @@ void StackLst::Push(const Complex& x) {
 }
 
 void StackLst::Pop() noexcept {
-  Node* temp = head_;
-  head_ = temp->next;
-  delete temp;
+  if (head_ != nullptr) {
+    Node* temp = head_;
+    head_ = temp->next;
+    delete temp;
+  }
 }
 
 bool StackLst::IsEmpty() noexcept {
@@ -34,11 +39,16 @@ StackLst::~StackLst() {
 }
 
 StackLst::StackLst(const StackLst& x) {
-  Node* t_head = x.head_;
-  while (t_head != nullptr) {
-    head_ = new Node;
-    head_->v = t_head->v;
-    head_->next = t_head->next;
-    t_head = t_head->next;
+  head_ = new Node;
+  head_->v = x.head_->v;
+  Node* new_head = head_;
+  Node* temp_head = x.head_;
+  while (temp_head->next != nullptr) {
+    new_head->next = new Node;
+    new_head = new_head->next;
+    temp_head = temp_head->next;
+    new_head->v = temp_head->v;
   }
+  new_head->next = nullptr;
+
 }
--- prj.test/test_stacklst.cpp
+++ prj.test/test_stacklst.cpp
@@ -3,7 +3,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-TEST_CASE("stackarr") {
+TEST_CASE("stacklst") {
   StackLst a;
   CHECK(a.IsEmpty() == true);
   a.Push(Complex(2, 5));
@@ -21,4 +21,24 @@ TEST_CASE("stackarr") {
   CHECK(a.Top() == Complex(3, 5));
   StackLst b(a);
   CHECK(b.Top() == a.Top());
+  a.Pop();
+  b.Pop();
+  CHECK(a.Top() == b.Top());
+  b.Pop();
+  CHECK(b.Top() == Complex(2, 5));
+  b.Pop();
+  CHECK_THROWS(b.Top());
+  a.Pop();
+  a.Pop();
+  CHECK_THROWS(a.Top());
+  a.Pop();
+  CHECK_THROWS(a.Top());
+  b.Push(Complex(7, 8));
+  b.Push(Complex(1, 2));
+  StackLst c(b);
+  c.Pop();
+  c.Push(Complex(11, 38));
+  CHECK(c.Top() != b.Top());
+  CHECK(c.Top() == Complex(11, 38));
+  CHECK(b.Top() == Complex(1, 2));
 }
